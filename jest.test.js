a771652bcade6ab0b71b7aaacdbe7fa83e4c217f
Leonnor <958978379@qq.com>
2022-09-16 17:03:54 +0800

    feat: Button组件

---
+++ jest.test.js
@@ -0,0 +1,4 @@
+test('test common matcher', () => {
+  expect(2 + 2).toBe(4)
+})
+
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Button, {ButtonType, ButtonSize} from "./components/Button/button";
 
 function App() {
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button disabled> Hello </Button>
+        <Button autoFocus> Hello </Button>
+        <Button btnType={ButtonType.Primary} size={ButtonSize.Large}> Hello </Button>
+        <Button btnType={ButtonType.Danger} size={ButtonSize.Small}> Hello </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" target="_blank"> Baidu </Button>
+        <Button btnType={ButtonType.Link} href="http://www.baidu.com" disabled> Baidu </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
